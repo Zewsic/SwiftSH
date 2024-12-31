9b4ddedf77720a800903745b8b9c52ebca47c2ff
Даниил Никишин <66947765+Zewsic@users.noreply.github.com>
2024-12-31 14:46:03 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "CSSH", url: "https://github.com/DimaRU/Libssh2Prebuild.git", from: "1.0.0")
+        .package(url: "https://github.com/DimaRU/Libssh2Prebuild.git", exact: "1.11.0-OpenSSL-1-1-1w")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
